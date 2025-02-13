eb080cf731be58d6d6ef09a0fdd7e007e1fd4e6e
DaLan <valkyrie777is@gmail.com>
2025-02-13 13:53:33 +0800

    修改05

--- routes/uploadImg.js
+++ routes/uploadImg.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const uploadMulter = require('../models/uploadMulter')
 const uploadController = require('../controllers/uploadController')
 
-// 上傳照片
+// 上傳圖檔
 router.post('/uploadImg', uploadMulter, uploadController.uploadImage);
+
+// 刪除圖檔
+router.post('/deleteImage', uploadMulter.deleteImage);
 
 module.exports = router;
